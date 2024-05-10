eaf279103c8bb649ecfc7d473298eb976715ba7e
Christian Weyer <cw@thinktecture.com>
2024-05-10 12:38:19 +0200

    More cleanup

--- dotnet8/wasiconsole/Program.cs
+++ dotnet8/wasiconsole/Program.cs
@@ -1,3 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 
-Console.WriteLine("Hello, Wasi Console!");
+Console.WriteLine("Hello, World - from .NET!");
+Console.WriteLine($"Running on {RuntimeInformation.OSArchitecture}");
---
+++ dotnet8/wasiconsole/wasi-host.mjs
@@ -0,0 +1,21 @@
+import { readFile } from 'node:fs/promises';
+import { WASI } from 'node:wasi';
+import { argv, env } from 'node:process';
+
+const wasi = new WASI({
+    version: 'preview1',
+    args: argv,
+    env,
+    preopens: {
+        '/': './'
+    },
+});
+
+const importObject = { wasi_snapshot_preview1: wasi.wasiImport };
+
+const wasm = await WebAssembly.compile(
+    await readFile(new URL('./bin/Debug/net8.0/wasi-wasm/AppBundle/wasiconsole-o.wasm', import.meta.url)),
+);
+const instance = await WebAssembly.instantiate(wasm, importObject);
+
+wasi.start(instance);
